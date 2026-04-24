63a7b99dc694da347c89d5603414f4919730cd57
leticiakq <leticiakq@al.insper.edu.br>
2026-04-24 15:19:21 -0300

    Question 2: Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,19 @@ def rolar_dados(n):
         resultado = random.randint(1, 6)
         resultados.append(resultado)
     return resultados   
+
+def guardar_dado(lista_rolados, lista_guardados, ind):
+    lista_rolados2 = []
+    lista_guardados2 = []
+
+    lista_guardados2 = lista_guardados
+    lista_guardados2.append(lista_rolados[ind])
+    del lista_rolados[ind]
+    lista_rolados2 = lista_rolados
+    resultado = [lista_rolados2, lista_guardados2]
+    return resultado
+      
+
+
+
+
